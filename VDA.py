5e4ef75e6c4101790db8a31a806a3fa4e1818b1d
Vannessa Adwoa  Darkowaa <vanadeps24@gmail.com>
2026-05-31 02:13:04 +0000

    Add files via upload

---
+++ VDA.py
@@ -0,0 +1,23 @@
+# 1. Blank line before the small initials
+print()
+
+# 2. Three periods followed by your small initials
+print("...VDA")
+
+# 3. Blank line between small and large initials
+print()
+
+# 4. The 10 lines that draw the large VDA letters (Each line is exactly 60 characters wide)
+print("...VV........VV..........DDDDDDDD..............AAAAAA..........")
+print("...VV........VV..........DDDDDDDDDD..........AAAAAAAAAA........")
+print("....VV......VV...........DD........DDD......AA........AA.......")
+print("....VV......VV...........DD..........DD....AA..........AA......")
+print(".....VV....VV............DD..........DD....AAAAAAAAAAAAAA......")
+print(".....VV....VV............DD..........DD....AAAAAAAAAAAAAA......")
+print("......VV..VV.............DD..........DD....AA..........AA......")
+print("......VV..VV.............DD........DDD.....AA..........AA......")
+print(".......VVVV......**......DDDDDDDDDD..**....AA..........AA....**")
+print("........VV.......**......DDDDDDDD....**....AA..........AA....**")
+
+# 5. Final blank line after the large initials
+print()
